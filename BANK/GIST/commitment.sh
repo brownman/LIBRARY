a4203c11ef629d717182937f3e7d249702ea373a
brownman <brownman556@gmail.com>
2014-11-22 12:57:24 +0200

    [dir_workspace] - manage switch_to and crontab

--- BANK/GIST/commitment.sh
+++ BANK/GIST/commitment.sh
@@ -23,6 +23,34 @@ use_sh string_to_buttons
 #$cmd_trap_err
 #print error use yad instead
 #exiting
+compute_line(){
+local line="${@:-}"
+local file=$file_done
+time_update
+local time1="${hour1}:${minute1}"
+
+if [ "$line" = exit ];then
+        xcowsay killing
+#        exit 1
+        #exiting 
+        commander kill "$$"
+#commander         exit 1
+    elif [ "$line" = delete ];then
+        echo -n '' > $file  
+
+    elif [ "$line" = edit ];then
+commander         gvim -f $0
+    else
+        #flite -t 'it is easy for me - you are a little monkey' &
+        local line_new="$line"
+file_update "$file" "$time1 _ $line_new"
+        #echo __${word}_
+#      commander   echo "$line"
+
+string_to_buttons_1  $line &
+    fi
+}
+
 set_env(){
 #commander ensure touch $file_done
 NAME=ofer
@@ -65,13 +93,15 @@ echo $delay
 dialog_sleep_1(){
 #local num=$( set_sleep ${1:-})
 #assert is_number "$num"
+#local hour2=0
 local sec="$1"
-shift
-local line="$@"
-commander time_update
-set -u
-let "hour2=$hour1 + 1"
-commander dialog_sleep $sec \"$hour1:$minute1 - $line\" \"$hour1 - $hour2\"
+local title0="$2"
+local line="$3"
+time_update
+local title="$hour1:$minute1 - $title0"
+#set -u
+#let "hour2 = $hour1 + 1"
+dialog_sleep $sec "$line" "$title"
 }
 
 intro_give(){
@@ -101,29 +131,32 @@ flite_1 'it is easy for a robot'
 
 
 run(){
-set_env
+local line  
+#set_env
 set -u
-local line_recent=''
-test -s $file_done && { set -o pipefail; line_recent=$(cat $file_done | tail -1 | cut -d' ' -f2- ); }
-print color 33 line_recent $line_recent
+local delay=$1
+local title="$2"
+local entry0="$3"
+xcowsay "MISSION: $title"
+#local line_recent=''
+#test -s $file_done && { set -o pipefail; line_recent=$(cat $file_done | tail -1 | cut -d'_' -f2- ); }
+#print color 33 line_recent $line_recent
 #exiting 
-sleep 1
-line=$( dialog_add_line $file_done  "$line_recent" ) 
-string_to_buttons_1  $line
+#sleep 1
+line=$( dialog_add_line $file_done "$title" "$entry0" ) 
+commander compute_line $line
+#echo $line
+
+dialog_sleep_1 $delay 'wish' "$line"
 }
 
 steps(){
-intro_robot
-run
-dialog_sleep_1 60 'give'
-intro_take
-run
-dialog_sleep_1 480 'take'
-intro_give
-run
-dialog_sleep_1 60 'give'
-point_up
-point_show
+  set_env
+  point_up
+  point_show
+run 100 'WISH' 'thanks '
+run 100 'TAKE' 'breakthrough '
+run 100 'EASY' 'maybe easier '
 }
 
 delay=${1:-60}
