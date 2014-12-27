c1e10469232afe818563ae48231ae42e90b77efc
brownman <brownman556@gmail.com>
2014-12-27 20:26:56 +0200

    Update travis.sh

--- travis.sh
+++ travis.sh
@@ -1,11 +1,20 @@
+install(){
+  ./install.sh
+}
 
+set_env(){
+    source /tmp/library.cfg
+}
 
-
-
-steps(){
-  source /tmp/library.cfg
+test1(){
   cat /tmp/target
   gxmessage_confirm a b c
+}
+
+steps(){
+  install
+  set_env
+  test1
 }
 
 
