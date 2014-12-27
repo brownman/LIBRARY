219b8c13691fcfcae19f8cde7747458d11ff9c8a
brownman <brownman556@gmail.com>
2014-12-27 20:48:48 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,8 +3,11 @@ pushd `dirname $0` >/dev/null
 
 exec 2> >( tee /tmp/err >&2)
 testing(){
- source library.cfg;
+source library.cfg;
 use print; print ok Library Activated!; 
+finder print
+finder translate sh
+pipe_translate dog
 }
 (set -e; testing ) || ( cat /tmp/err )
 
